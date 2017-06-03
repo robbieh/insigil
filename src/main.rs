f6ff98d2ebf77120d27f52a7cad5d3f611c62577
Robbie Huffman <robbie.huffman@gmail.com>
2017-06-02 21:18:54 -0400

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,19 @@ extern crate sdl2;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::video::{Window, WindowContext};
+use sdl2::render::{Canvas, Texture, TextureCreator};
 use std::time::Duration;
 use std::thread;
 
-fn ring(window: sdl2::video::Window) {
-    let (width: u32, height: u32) = window.size()
+fn ring(canvas: &mut Canvas<Window>) {
+    let (width, height) = canvas.window().size();
+    //print!("width: {:?}, height: {:?}", width, height);
+
+
+
+
+
 }
 
 pub fn main() {
@@ -30,13 +38,15 @@ pub fn main() {
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit {..} | Event::KeyDown { 
+                    keycode: Some(Keycode::Escape), ..
+                } => {
                     break 'running
                 },
                 _ => {}
             }
         }
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-        ring(window)
+        ring(&mut canvas)
     }
 }
