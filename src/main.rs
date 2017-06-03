80613d3cffcd0400d8f3a73f724873b66f6f96c7
Robbie Huffman <robbie.huffman@gmail.com>
2017-06-02 23:30:03 -0400

    it draws a little...now need data types

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,38 @@ use sdl2::gfx::primitives::DrawRenderer;
 
 use std::time::Duration;
 use std::thread;
+use std::collections::HashMap;
+
+// need... a data structure to fill from the thread
+// what data types? 
+// series of ints
+//      interpreted as ...
+//          histogram - 'ticks' of different heights ||||
+//          interval - space between ticks   | |   |  |    |  | |
+
+enum 
+
+macro_rules! rect(
+    ($x:expr, $y:expr, $w:expr, $h:expr) => (
+        sdl2::rect::Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
+    )
+);
+
+
 
 fn ring(canvas: &mut Canvas<Window>) {
     let (width, height) = canvas.window().size();
+    let half_height: i32 = height as i32 / 2;
+    let half_width: i32 = width as i32 / 2;
     //print!("width: {:?}, height: {:?}", width, height);
 
+    //let r = rect!(half_width, half_height, width, height);
+
+    //print!("{:?},{:?} {:?},{:?}", width, height, half_height, half_width);
+
     let color = Color::RGB(0,255,0);
-    let _ = canvas.circle(0,0,10,color);
-    let _ = canvas.circle(0,0,100,color);
-    let _ = canvas.circle(0,0,1000,color);
+    //let _ = canvas.set_viewport(r);
+    let _ = canvas.circle(half_width as i16,half_height as i16,50,color);
     canvas.present();
 
 }
