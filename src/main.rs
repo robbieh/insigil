4604ce78dfcc24745b6d69516df26dc9ac3b0b23
Robbie Huffman <robbie.huffman@gmail.com>
2017-06-30 19:05:56 -0400

    Trying Piston

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-extern crate sdl2;
+extern crate piston_window;
 
-use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::video::{Window, WindowContext};
-use sdl2::render::{Canvas, Texture, TextureCreator};
-
-use sdl2::gfx::primitives::DrawRenderer;
-use sdl2::rect::Rect;
+use piston_window::*;
 
 use std::time::Duration;
 use std::thread;
@@ -75,45 +68,25 @@ fn ring(canvas: &mut Canvas<Window>, viztype: &mut RingVizType) {
 
 fn io_reader(textq: VecDeque<char>) {
     for line in std::io::stdin().lines() {
-            println!(line.ok().unwrap());
+            //println!(line.ok().unwrap());
             //line.ok().unwrap().each()
     }
 }
 
 pub fn main() {
-    let textq: VecDeque <char> = VecDeque:::new();
+    let textq: VecDeque <char> = VecDeque::new();
     thread::spawn(|| { io_reader(); });
 
     let mut viztype = RingVizType::Hist;
 
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} | Event::KeyDown { 
-                    keycode: Some(Keycode::Escape), ..
-                } => {
-                    break 'running
-                },
-                _ => {}
-            }
-        }
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-        ring(&mut canvas, &mut viztype);
+    let mut window: PistonWindow = WindowSettings::nwe("twirl", [640,640])
+        .exit_on_esc(true).build().unwrap();
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c,g| {
+        clear([1.0; 4], g);
+        rectangle([1.0, 0.0, 0.0, 1.0]
+                  [0.0, 0.0, 100.0, 100.0],
+                  c.transform,g);
+        });
     }
 }
